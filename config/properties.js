485a3a0ccc8f98105fb0a1000aff7cd9a72b7346
Luis <gerard95@live.com.mx>
2020-06-03 21:17:43 -0500

    se modifican endpoints para produccion

--- config/properties.js
+++ config/properties.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     HOST: 'localhost', //database
     DB: 'superff', // database
-    BACKEND_URL: 'http://localhost:3000',
-    FRONTEND_URL: 'http://localhost:4200',
+    BACKEND_URL: 'https://superflyfitnessapi.herokuapp.com',
+    FRONTEND_URL: 'https://superflyfitness.herokuapp.com/',
     VIMEO_API: 'https://api.vimeo.com/videos/'
 }
